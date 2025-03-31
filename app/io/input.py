7d4942e0da028d9c9cb94ca17711d8dbdbb77304
bergamontt <dariamumbo@gmail.com>
2025-03-31 16:12:11 +0300

    Added Empty IO functions

---
+++ app/io/input.py
@@ -0,0 +1,9 @@
+
+def read_input_from_console():
+    pass
+
+def read_file_with_python(filename):
+    pass
+
+def read_file_with_pandas(filename):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def write_output_in_console(output = '\n'):
+    pass
+
+def write_to_file_with_python(filename, output):
+    pass
